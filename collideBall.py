ebfec4145ba5adf878eb9a674c8d8c204c8b002d
kai-1234 <74052281+kai-1234@users.noreply.github.com>
2021-01-08 19:30:35 +0800

    Update collideBall.py

--- collideBall.py
+++ collideBall.py
@@ -1,11 +1,12 @@
 import pygame
 import random
+import math
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self,speed,srx,sry,r,color):
         super().__init__(self)
         self.x = srx
-        self.y=sry
+        self.y = sry
         self.image=pygame.Surface([2*r,2*r])
         self.image.fill(BLACK)
         pygame.draw.circle(self.image,color,(r,r),r)
@@ -14,10 +15,16 @@ class Ball(pygame.sprite.Sprite):
         direction = random.randint(20,70)  #移動角度
         radian = math.radians(direction)  #角度轉為弳度
         self.dx = speed * math.cos(radian)  #球水平運動速度
-        self.dy = -speed * math.sin(radian)
+        self.dy = speed * math.sin(radian)
     def update(self):
-        
-
+        self.x+=self.dx
+        self.rect.x=self.x
+        if self.rect.left<0 or self.rect.right>screen.get_width():
+            self.dx*=-1
+        self.y+=self.dy
+        self.rect.y=self.y
+        if self.rect.top<0 or self.rect.bottom>screen.get_height():
+            self.dy*=-1
         
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
